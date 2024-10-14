127f25b33119e5972a3a724075fa548294d8def0
Eddydev-ALU <e.irasetsa@alustudent.com>
2024-10-14 13:06:38 -0700

    Created Archive script file

--- archive_log.sh
+++ archive_log.sh
@@ -4,9 +4,9 @@ log_file="heart_rate_log.txt"
 time_stamp=$(date "+%Y%m%d_%H%M%S")
 if [ -f $log_file ]
 then 
-	echo "File Exists"
+	echo "Archive File Created"
 arch_file="${log_file}_${time_stamp}"
 mv "$log_file" "$arch_file"
 else
-	echo "Archive File created"
+	echo "File does not exist"
 fi
