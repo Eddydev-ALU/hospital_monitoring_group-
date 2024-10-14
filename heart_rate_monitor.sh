840916a1eca677a74c8ff487d21d32048ca9fc59
jmuhire13 <jmuhire@alustudent.com>
2024-10-14 12:10:12 -0700

    Made_functionality

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -3,5 +3,14 @@
 echo "Enter Device Name:"
 read device_name
 
-logFile ="heart_rate_log.txt"
+logFile="heart_rate_log.txt"
 
+while true;
+do
+  Timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+  Heart_rate=$((RANDOM % 40 + 60))  # Random value between 60-100
+  echo "$Timestamp $device_name $Heart_rate" >> "$logFile"
+  sleep 1
+done
+
+echo "Heart rate monitor running in the background. Process ID (PID): $!"
