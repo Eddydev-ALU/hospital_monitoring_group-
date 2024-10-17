db141b127419c8d9b255230b9078eb19be15683e
nehemy55 <n.ishimwe5@alustudent.com>
2024-10-17 13:59:41 +0200

    backing up archive

--- backup_archives.sh
+++ backup_archives.sh
@@ -4,15 +4,12 @@
 REMOTE_USER="03e282cfdac0"
 REMOTE_HOST="03e282cfdac0.7d5f7213.alu-cod.online"
 REMOTE_PATH="/home"
-# Attention!! The three variables above are my intranet_sandbox parameters. You must replace them by yours!
 
-# Execute the backup using scp
 mkdir ./archived_logs_group5
 mv heart_rate_log.txt_* archived_logs_group5/
 
 scp -r archived_logs_group5 "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
 
-# Check if the scp command was successful
 if [[ $? -eq 0 ]]; then
     echo "Files Backed-up successfully!!"
     echo "Your backup location: $REMOTE_PATH"
